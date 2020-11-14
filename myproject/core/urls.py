4ef6de1af3d64f48957268c38b5879798271ee7e
Regis Santos <rg3915@yahoo.com.br>
2020-11-14 17:57:45 -0300

    Refactoring pages

--- myproject/core/urls.py
+++ myproject/core/urls.py
@@ -1,10 +1,49 @@
-from django.urls import path
+from django.urls import include, path
 from myproject.core import views as v
 
 
 app_name = 'core'
 
 
+url_coreui = [
+    path('breadcrumb/', v.breadcrumb, name='breadcrumb'),
+    path('cards/', v.cards, name='cards'),
+    path('carousel/', v.carousel, name='carousel'),
+    path('collapse/', v.collapse, name='collapse'),
+    path('forms/', v.forms, name='forms'),
+    path('jumbotron/', v.jumbotron, name='jumbotron'),
+    path('list_group/', v.list_group, name='list_group'),
+    path('navs/', v.navs, name='navs'),
+    path('pagination/', v.pagination, name='pagination'),
+    path('popovers/', v.popovers, name='popovers'),
+    path('progress/', v.progress, name='progress'),
+    path('scrollspy/', v.scrollspy, name='scrollspy'),
+    path('switches/', v.switches, name='switches'),
+    path('tables/', v.tables, name='tables'),
+    path('tabs/', v.tabs, name='tabs'),
+    path('tooltips/', v.tooltips, name='tooltips'),
+    path('brand_buttons/', v.brand_buttons, name='brand_buttons'),
+    path('button_group/', v.button_group, name='button_group'),
+    path('buttons/', v.buttons, name='buttons'),
+    path('dropdowns/', v.dropdowns, name='dropdowns'),
+    path('charts/', v.charts, name='charts'),
+    path('colors/', v.colors, name='colors'),
+    path('coreui_icons/', v.coreui_icons, name='coreui_icons'),
+    path('flags/', v.flags, name='flags'),
+    path('font_awesome/', v.font_awesome, name='font_awesome'),
+    path('simple_line_icons/', v.simple_line_icons, name='simple_line_icons'),
+    path('login/', v.login, name='login'),
+    path('alerts/', v.alerts, name='alerts'),
+    path('badge/', v.badge, name='badge'),
+    path('modals/', v.modals, name='modals'),
+    path('register/', v.register, name='register'),
+    path('typography/', v.typography, name='typography'),
+    path('widgets/', v.widgets, name='widgets'),
+    path('404/', v.error404, name='error404'),
+    path('500/', v.error500, name='error500'),
+]
+
 urlpatterns = [
     path('', v.index, name='index'),
+    path('coreui/', include(url_coreui)),
 ]
--- myproject/core/views.py
+++ myproject/core/views.py
@@ -3,5 +3,179 @@ from django.shortcuts import render
 
 def index(request):
     template_name = 'index.html'
-    context = {}
-    return render(request, template_name, context)
+    return render(request, template_name)
+
+
+def breadcrumb(request):
+    template_name = 'base/breadcrumb.html'
+    return render(request, template_name)
+
+
+def cards(request):
+    template_name = 'base/cards.html'
+    return render(request, template_name)
+
+
+def carousel(request):
+    template_name = 'base/carousel.html'
+    return render(request, template_name)
+
+
+def collapse(request):
+    template_name = 'base/collapse.html'
+    return render(request, template_name)
+
+
+def forms(request):
+    template_name = 'base/forms.html'
+    return render(request, template_name)
+
+
+def jumbotron(request):
+    template_name = 'base/jumbotron.html'
+    return render(request, template_name)
+
+
+def list_group(request):
+    template_name = 'base/list-group.html'
+    return render(request, template_name)
+
+
+def navs(request):
+    template_name = 'base/navs.html'
+    return render(request, template_name)
+
+
+def pagination(request):
+    template_name = 'base/pagination.html'
+    return render(request, template_name)
+
+
+def popovers(request):
+    template_name = 'base/popovers.html'
+    return render(request, template_name)
+
+
+def progress(request):
+    template_name = 'base/progress.html'
+    return render(request, template_name)
+
+
+def scrollspy(request):
+    template_name = 'base/scrollspy.html'
+    return render(request, template_name)
+
+
+def switches(request):
+    template_name = 'base/switches.html'
+    return render(request, template_name)
+
+
+def tables(request):
+    template_name = 'base/tables.html'
+    return render(request, template_name)
+
+
+def tabs(request):
+    template_name = 'base/tabs.html'
+    return render(request, template_name)
+
+
+def tooltips(request):
+    template_name = 'base/tooltips.html'
+    return render(request, template_name)
+
+
+def brand_buttons(request):
+    template_name = 'buttons/brand-buttons.html'
+    return render(request, template_name)
+
+
+def button_group(request):
+    template_name = 'buttons/button-group.html'
+    return render(request, template_name)
+
+
+def buttons(request):
+    template_name = 'buttons/buttons.html'
+    return render(request, template_name)
+
+
+def dropdowns(request):
+    template_name = 'buttons/dropdowns.html'
+    return render(request, template_name)
+
+
+def charts(request):
+    template_name = 'charts.html'
+    return render(request, template_name)
+
+
+def colors(request):
+    template_name = 'colors.html'
+    return render(request, template_name)
+
+
+def coreui_icons(request):
+    template_name = 'icons/coreui-icons.html'
+    return render(request, template_name)
+
+
+def flags(request):
+    template_name = 'icons/flags.html'
+    return render(request, template_name)
+
+
+def font_awesome(request):
+    template_name = 'icons/font-awesome.html'
+    return render(request, template_name)
+
+
+def simple_line_icons(request):
+    template_name = 'icons/simple-line-icons.html'
+    return render(request, template_name)
+
+
+def alerts(request):
+    template_name = 'notifications/alerts.html'
+    return render(request, template_name)
+
+
+def badge(request):
+    template_name = 'notifications/badge.html'
+    return render(request, template_name)
+
+
+def modals(request):
+    template_name = 'notifications/modals.html'
+    return render(request, template_name)
+
+
+def typography(request):
+    template_name = 'typography.html'
+    return render(request, template_name)
+
+
+def widgets(request):
+    template_name = 'widgets.html'
+    return render(request, template_name)
+
+
+def login(request):
+    template_name = 'login.html'
+    return render(request, template_name)
+
+
+def register(request):
+    template_name = 'register.html'
+    return render(request, template_name)
+
+
+def error404(request):
+    template_name = '404.html'
+    return render(request, template_name)
+
+
+def error500(request):
+    template_name = '500.html'
+    return render(request, template_name)
